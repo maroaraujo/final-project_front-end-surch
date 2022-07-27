9e8b64f7664d4698bb62c0221e407f99680ae2cb
IsmailAli <ismailali98@gmail.com>
2022-07-27 16:52:30 +0100

    Finally have a functioning next js pwa

--- reconnect/next.config.js
+++ reconnect/next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa');
+module.exports = withPWA({
   reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+  pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true
+  }
+})
