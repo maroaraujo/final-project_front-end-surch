5ccd2dd21ffa4c80cb9a57ffc1ed563a481d840e
maroaraujo <tiagomoller@192.168.1.7>
2022-08-01 13:14:48 +0300

    welcome css is separated

--- reconnect/pages/Welcome.js
+++ reconnect/pages/Welcome.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/welcome.module.css";
+import stylesHome from "../styles/Home.module.css";
 import DisplayLogo from "../component/DisplayLogo/DisplayLogo";
 import happysun from "../public/images/happy sun.png"
+import Link from "next/link"
+
 
 
 export default function Welcome() {
@@ -16,13 +19,14 @@ export default function Welcome() {
 
       <main className={styles.welcomemain}>
         <DisplayLogo />
-        <h1 className={styles.title}>
+        <h1 className={stylesHome.title}>
         Hi Guest, <br />
         Reconnect with yourself
         </h1>
 <h4>Take a deep breath...</h4>
         <Image className={styles.sun} src={happysun} alt="Welcome Happy Sun" ></Image>
-        <button className={styles.getstarted}>GET STARTED</button>
+        <button className={styles.getstarted}>
+        <Link href="https://reconnect-surch.netlify.app/home">GET STARTED</Link></button>
       </main>
     </div>
   );
