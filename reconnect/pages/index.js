d35602d087c26825afd5528c4db66ca3924914ae
leigheastell <leigheastell1286@gmail.com>
2022-08-01 10:12:58 +0100

    auth0 login/logout done

--- reconnect/pages/index.js
+++ reconnect/pages/index.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
+import Login from "../component/Login/login";
 
-export default function signup() {
+
+export default function Signup() {
+  const { user, error, isLoading } = useUser();
     return (
     <main>    
     <h1>Sign Up Page</h1>
-    <Link href="/api/auth/login">Login</Link>
-    <Link href="/api/auth/logout">Logout</Link>
+    <Login/>
+    
     </main>
     );
     }
