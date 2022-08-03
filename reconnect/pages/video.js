b5aef10e279786445456f521998cc515efe30252
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-03 16:01:37 +0100

    following netlify errors to fix deployment issues

--- reconnect/pages/video.js
+++ reconnect/pages/video.js
@@ -1,32 +1,26 @@
-import ReactPlayer from 'react-player';
-import meditation from '../library/videoList';
-import { useState } from 'react';
+import ReactPlayer from "react-player";
+import meditation from "../library/videoList";
+import { useState } from "react";
 
 export default function Video() {
-    
-    const [state, setState]= useState(initialState)
-    const initialState = {
-        url: "",
-        pip: false,
-        playing:true, 
-        controls: false, 
-        light: true, 
-        played: 0, 
-        loaded: 0
-    }
+  const [state, setState] = useState(initialState);
+  const initialState = {
+    url: "hello",
+    pip: false,
+    playing: true,
+    controls: false,
+    light: true,
+    played: 0,
+    loaded: 0,
+  };
 
-    load = url => {
+  // load = url => {
 
-    }
+  // }
 
-    
-        
-
-    return (
-        <>
-        <ReactPlayer url="https://www.youtube.com/watch?v=O-6f5wQXSu8" />
-        
-        </>
-    )
-    
+  return (
+    <>
+      <ReactPlayer url="https://www.youtube.com/watch?v=O-6f5wQXSu8" />
+    </>
+  );
 }
