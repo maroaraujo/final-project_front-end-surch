4cd8377df4c66991b0acf64a39f7f211e0e9a4a1
leigheastell <leigheastell1286@gmail.com>
2022-08-04 11:11:25 +0100

    meditation page created

--- reconnect/pages/meditation.js
+++ reconnect/pages/meditation.js
@@ -1,11 +1,12 @@
 //import ReactPlayer from 'react-player';
 //import { useState, useEffect } from 'react';
-//import BackButton from "../component/Button/Button";
+import BackButton from "../component/BackButton/BackButton";
 import { useRouter } from "next/router";
 import stylesvideo from "../styles/video.module.css";
 import Image from 'next/image';
 import videoList from '../library/videoList';
 import styles from "../styles/Home.module.css";
+import CloudyBackground from "../CloudyBackground/CloudyBackground";
 
 
 
@@ -21,19 +22,41 @@ export default function Meditation () {
 
     return (
     <div className={styles.main}>
-        
+        <BackButton url={"https://reconnect-surch.netlify.app/home"}/>
         <h1 className={styles.title}>Meditation</h1>
+        <CloudyBackground/>
+        <div className={stylesvideo.background}>
         <Image src='https://i.ytimg.com/vi/O-6f5wQXSu8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBajCqSN4D6cgnZT8SnZ-RcV0ILNg' alt='10 minutes meditation video thumbnail' layout={imageSize.layout} height={imageSize.height} width={imageSize.width} onClick={onClick}/>
-        <div className = {stylesvideo.description}>
-        <p>10-Minute Meditation For Anxiety</p>
+        
+        <h4 className={stylesvideo.h4}>10-Minute Meditation For Anxiety</h4>
+        <p className={stylesvideo.p}>Brief description of the video lives here...</p>
         </div>
+        <div>
+        <Image src='https://i.ytimg.com/vi/O-6f5wQXSu8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBajCqSN4D6cgnZT8SnZ-RcV0ILNg' alt='10 minutes meditation video thumbnail' layout={imageSize.layout} height={imageSize.height} width={imageSize.width} onClick={onClick}/>
+        
+        <h4 className={stylesvideo.h4}>10-Minute Meditation For Anxiety</h4>
+        <p className={stylesvideo.p}>Brief description of the video lives here...</p>
+        </div>
+        <div>
+        <Image src='https://i.ytimg.com/vi/O-6f5wQXSu8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBajCqSN4D6cgnZT8SnZ-RcV0ILNg' alt='10 minutes meditation video thumbnail' layout={imageSize.layout} height={imageSize.height} width={imageSize.width} onClick={onClick}/>
+        
+        <h4 className={stylesvideo.h4}>10-Minute Meditation For Anxiety</h4>
+        <p className={stylesvideo.p}>Brief description of the video lives here...</p>
+        </div>
+        <div>
+        <Image src='https://i.ytimg.com/vi/O-6f5wQXSu8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBajCqSN4D6cgnZT8SnZ-RcV0ILNg' alt='10 minutes meditation video thumbnail' layout={imageSize.layout} height={imageSize.height} width={imageSize.width} onClick={onClick}/>
+        
+        <h4 className={stylesvideo.h4}>10-Minute Meditation For Anxiety</h4>
+        <p className={stylesvideo.p}>Brief description of the video lives here...</p>
+        </div>
+        <footer className={stylesvideo.footer}/>
     </div>
     )
     }
 
-    //This will need to go back in just before h1 before merging, also remove other comments at import
-    //<BackButton url={"https://reconnect-surch.netlify.app/home"}/>
 
+    //This will need to go back in just before h1 before merging, also remove other comments at import
+//<div className = {stylesvideo.description}/>
 
 
 
