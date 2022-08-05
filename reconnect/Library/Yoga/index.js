dc52ef199fc6521ef64c8efcf35d940778f2813c
wanteng <wantengteoh@gmail.com>
2022-08-05 17:38:52 +0100

    fixed yoga imgUrl

--- reconnect/Library/Yoga/index.js
+++ reconnect/Library/Yoga/index.js
@@ -1,11 +1,11 @@
 import { nanoid } from 'nanoid';
 
 let yoga = [
-    {imgUrl:'https://i.ytimg.com/an_webp/ZiQh8jA5tVM/mqdefault_6s.webp?du=3000&sqp=CNGHs5cG&rs=AOn4CLAYGjSt-JGtNo8UU4-Nj04kfchkQw',
+    {imgUrl:'https://i.ytimg.com/an_webp/ZiQh8jA5tVM/mqdefault_6s.webp?du=3000&sqp=CMrwtJcG&rs=AOn4CLA9KU4qA3tOLI3atTa-ZkPlNufyzA',
      videoUrl:'https://www.youtube.com/watch?v=ZiQh8jA5tVM',
      title:'Healthy Body Yoga - Yoga With Adriene'},
      
-     {imgUrl:'https://i.ytimg.com/an_webp/s-7lyvblFNI/mqdefault_6s.webp?du=3000&sqp=CN6Qs5cG&rs=AOn4CLC09CJumjw74EIwOBQ1XvjKScw23w',
+     {imgUrl:'https://i.ytimg.com/an_webp/s-7lyvblFNI/mqdefault_6s.webp?du=3000&sqp=CObotJcG&rs=AOn4CLBpRzWlo87V9oUZ5jWyQCHVvXEqHg',
      videoUrl:'https://www.youtube.com/watch?v=s-7lyvblFNI',
      title:'Stretches for Neck, Shoulder & Upper Back Pain Relief | 10 min. Yoga to release Tension and Relax'}
 
