6f428fcd2defaedcb25fbc4ad0e33e8af2efa937
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-08 09:50:53 +0100

    added back button to settings

--- reconnect/pages/settings.js
+++ reconnect/pages/settings.js
@@ -2,33 +2,33 @@ import { useUser } from "@auth0/nextjs-auth0";
 import Logout from "../component/Logout/logout";
 import TabBar from "../component/TabBar/TabBar";
 import Image from "next/image";
-import backgroundImage from "../public/images/SettingsBackGround.png"
-import styles from "../styles/Settings.module.css"
+import backgroundImage from "../public/images/SettingsBackGround.png";
+import styles from "../styles/Settings.module.css";
+import BackButton from "../component/BackButton/BackButton";
 
 export default function Settings() {
   const { user, error, isLoading } = useUser();
   return (
     <main className={styles.title}>
-    <h1>Settings</h1>
-    <div className={styles.backgroundImage}>
-      <Image
-        alt="SettingsBackground"
-        src={backgroundImage}
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-      />
-
-    </div>
+      <BackButton url={"/home"} />
+      <h1>Settings</h1>
+      <div className={styles.backgroundImage}>
+        <Image
+          alt="SettingsBackground"
+          src={backgroundImage}
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
 
       <div className={styles.body}>
+        <h3>Notifications</h3>
 
-    <h3>Notifications</h3>
+        <h3>Dark Mode</h3>
 
-    <h3>Dark Mode</h3>
-
-      {user ? <Logout /> : null} 
+        {user ? <Logout /> : null}
       </div>
-      </main>
+    </main>
   );
 }
