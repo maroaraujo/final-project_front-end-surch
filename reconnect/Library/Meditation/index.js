5248ab43be630121b0518eab0f4f6e3a138d623f
Maria <maroaraujo@gmail.com>
2022-08-11 12:50:56 +0300

    added images and some videos on meditation

--- reconnect/Library/Meditation/index.js
+++ reconnect/Library/Meditation/index.js
@@ -1,17 +1,52 @@
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
+import anxiety from "../../public/images/anxiety.jpeg";
+import feelingnew from "../../public/images/feelingnew.jpeg";
+import anxietyyellow from "../../public/images/anxietyyellow.jpeg";
+import energy from "../../public/images/energy.jpeg";
+import positive from "../../public/images/positive.jpeg";
+import sleep from "../../public/images/meditationsleep.jpeg";
 
 let meditation = [
-    {imgUrl : 'https://i.ytimg.com/vi/O-6f5wQXSu8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBajCqSN4D6cgnZT8SnZ-RcV0ILNg',
-    videoUrl : 'https://www.youtube.com/watch?v=O-6f5wQXSu8',
-    title : '10-Minute Meditation For Anxiety' },
+  {
+    imgUrl: anxiety,
+    videoUrl: "https://www.youtube.com/watch?v=penUy-GhApQ",
+    title: "10 Minute Meditation for Anxiety",
+  },
 
-    {imgUrl : 'https://i.ytimg.com/an_webp/ZToicYcHIOU/mqdefault_6s.webp?du=3000&sqp=CJi3sJcG&rs=AOn4CLBXw2jZhdHXEwkpe4Uv7o2iFj1OGQ',
-    videoUrl : 'https://www.youtube.com/watch?v=ZToicYcHIOU',
-    title : 'Daily Calm | 10 Minute Mindfulness Meditation | Be Present'}
-    
-]
+  {
+    imgUrl: energy,
+    videoUrl: "https://www.youtube.com/watch?v=tMc9uxDYGJY",
+    title: "Cleanse Your Energy (10 Minute Meditation Guided)",
+  },
 
-meditation.forEach(e => {e.id=nanoid();
-e.isFavourite=false;});
+  {
+    imgUrl: sleep,
+    videoUrl: "https://www.youtube.com/watch?v=aEqlQvczMJQ",
+    title: "10-Minute Meditation For Sleep",
+  },
 
-export default meditation
+  {
+    imgUrl: feelingnew,
+    videoUrl: "https://www.youtube.com/watch?v=xE4MYIQ1sCA",
+    title: "Feeling Like A Brand New You ~ 10 Minute Guided Meditation",
+  },
+
+  {
+    imgUrl: anxietyyellow,
+    videoUrl: "https://www.youtube.com/watch?v=O-6f5wQXSu8",
+    title: "10-Minute Meditation For Anxiety",
+  },
+
+  {
+    imgUrl: positive,
+    videoUrl: "https://www.youtube.com/watch?v=lJKMYBzI9RE",
+    title: "10 Minute Positive Energy Meditation",
+  },
+];
+
+meditation.forEach((e) => {
+  e.id = nanoid();
+  e.isFavourite = false;
+});
+
+export default meditation;
