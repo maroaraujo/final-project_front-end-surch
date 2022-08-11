b7bad5c4cdc1d72dea1389b101e1d50dfc143cc4
leigheastell <leigheastell1286@gmail.com>
2022-08-11 10:55:23 +0100

    changed the url links

--- reconnect/Library/Stretching/index.js
+++ reconnect/Library/Stretching/index.js
@@ -1,11 +1,13 @@
 import { nanoid } from 'nanoid';
+import stretching1 from "../../public/images/Stretching1.jpg";
+import stretching2 from "../../public/images/stretching2.jpg"
 
 let stretching = [
-    {imgUrl:'https://i.ytimg.com/an_webp/g_tea8ZNk5A/mqdefault_6s.webp?du=3000&sqp=CKDvtJcG&rs=AOn4CLAgGCj-A2Gxt27sa3iSBbZCnGresQ',
+    {imgUrl:stretching1,
      videoUrl:'https://www.youtube.com/watch?v=g_tea8ZNk5A',
      title:'15 Min. Full Body Stretch | Daily Routine for Flexibility, Mobility & Relaxation | DAY 7'},
 
-     {imgUrl:'https://i.ytimg.com/an_webp/t1rRo6cgM_E/mqdefault_6s.webp?du=3000&sqp=CID9tJcG&rs=AOn4CLA_uFtep8yuh6Z66qK40RLgjddIfw',
+     {imgUrl:stretching2,
      videoUrl:'https://www.youtube.com/watch?v=5Ju3XvZ6S1Q',
      title:'15 Min Yoga FULL BODY STRETCH & Deep Relaxation'}
      
